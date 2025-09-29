04530a15d89ee1c206311563a18ad244a2931c09
shyam sundar B <shyam@shyams-MacBook-Air.local>
2025-09-30 04:12:35 +0530

    added_outbox_processor

---
+++ hooks/prisma/migrations/20250929223902_added_sorting_order/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."Action" ADD COLUMN     "sortingOrder" INTEGER NOT NULL DEFAULT 0;
--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,27 +1,36 @@
 import express from 'express'
-import { PrismaClient } from './generated/prisma/index.js';
+import { PrismaClient} from "@prisma/client"       
+
 const app=express()
-const client=new PrismaClient
+app.use(express.json());
+const client=new PrismaClient();
 app.post('/hooks/catch/:userId/:zapId',async (req,res)=>{
     const userId=req.params.userId;
     const zapId= req.params.zapId;
     const body =req.body;
     await client.$transaction(async tx =>{
-        const run= await client.zapRun.create({
+        const run= await tx.zapRun.create({
             data:{
                 zapId:zapId,
                 metadata:body
             }
         })
-        await client.zapRunOutbox.create({
+        await tx.zapRunOutbox.create({
             data :{
                 zapRunid:run.id
             }
         })
 
+
+    })
+    res.json({
+        "messafe":"sdfiojdas"
     })
     
     
 }
 
 )
+app.listen(3000,()=>{
+    console.log("serber is running")
+})
